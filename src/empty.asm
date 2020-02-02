d217bf277e7d15bec5c9ba85edd75f0f07a3f4f8
Boris Vinogradov <no111u3@gmail.com>
2020-02-02 19:17:54 +0300

    Fix ints table in template project

--- src/empty.asm
+++ src/empty.asm
@@ -12,33 +12,32 @@
 ;****************************************************************
 .cseg
 .org 0x000
-		rjmp reset
-;		rjmp EXT_INT0 ; IRQ0 Handler
-;		rjmp EXT_INT1 ; IRQ1 Handler
-;		rjmp PCINT0 ; PCINT0 Handler
-;		rjmp PCINT1 ; PCINT1 Handler
-;		rjmp PCINT2 ; PCINT2 Handler
-;		rjmp WDT ; Watchdog Timer Handler
-;		rjmp TIM2_COMPA ; Timer2 Compare A Handler
-;		rjmp TIM2_COMPB ; Timer2 Compare B Handler
-;		rjmp TIM2_OVF ; Timer2 Overflow Handler
-;		rjmp TIM1_CAPT ; Timer1 Capture Handler
-;		rjmp TIM1_COMPA ; Timer1 Compare A Handler
-;		rjmp TIM1_COMPB ; Timer1 Compare B Handler
-;		rjmp TIM1_OVF ; Timer1 Overflow Handler
-;		rjmp TIM0_COMPA ; Timer0 Compare A Handler
-;		rjmp TIM0_COMPB ; Timer0 Compare B Handler
-;		rjmp TIM0_OVF ; Timer0 Overflow Handler
-;		rjmp SPI_STC ; SPI Transfer Complete Handler
-;		rjmp USART_RXC ; USART, RX Complete Handler
-;		rjmp USART_UDRE ; USART, UDR Empty Handler
-;		rjmp USART_TXC ; USART, TX Complete Handler
-;		rjmp ADC ; ADC Conversion Complete Handler
-;		rjmp EE_RDY ; EEPROM Ready Handler
-;		rjmp ANA_COMP ; Analog Comparator Handler
-;		rjmp TWI ; 2-wire Serial Interface Handler
-;		rjmp SPM_RDY ; Store Program Memory Ready Handler
-
+		rjmp reset 	; Reset Handler
+		reti 		;		rjmp EXT_INT0 ; IRQ0 Handler
+		reti 		;		rjmp EXT_INT1 ; IRQ1 Handler
+		reti 		;		rjmp PCINT0 ; PCINT0 Handler
+		reti 		;		rjmp PCINT1 ; PCINT1 Handler
+		reti 		;		rjmp PCINT2 ; PCINT2 Handler
+		reti 		;		rjmp WDT ; Watchdog Timer Handler
+		reti 		;		rjmp TIM2_COMPA ; Timer2 Compare A Handler
+		reti 		;		rjmp TIM2_COMPB ; Timer2 Compare B Handler
+		reti 		;		rjmp TIM2_OVF ; Timer2 Overflow Handler
+		reti 		;		rjmp TIM1_CAPT ; Timer1 Capture Handler
+		reti 		;		rjmp TIM1_COMPA ; Timer1 Compare A Handler
+		reti 		;		rjmp TIM1_COMPB ; Timer1 Compare B Handler
+		reti 		;		rjmp TIM1_OVF ; Timer1 Overflow Handler
+		reti 		;		rjmp TIM0_COMPA ; Timer0 Compare A Handler
+		reti 		;		rjmp TIM0_COMPB ; Timer0 Compare B Handler
+		reti 		;		rjmp TIM0_OVF ; Timer0 Overflow Handler
+		reti 		;		rjmp SPI_STC ; SPI Transfer Complete Handler
+		reti 		;		rjmp USART_RXC ; USART, RX Complete Handler
+		reti 		;		rjmp USART_UDRE ; USART, UDR Empty Handler
+		reti 		;		rjmp USART_TXC ; USART, TX Complete Handler
+		reti 		;		rjmp ADC ; ADC Conversion Complete Handler
+		reti 		;		rjmp EE_RDY ; EEPROM Ready Handler
+		reti 		;		rjmp ANA_COMP ; Analog Comparator Handler
+		reti 		;		rjmp TWI ; 2-wire Serial Interface Handler
+		reti 		;		rjmp SPM_RDY ; Store Program Memory Ready Handler
 reset:
 		sset RAMEND
 start:
