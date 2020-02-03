d9793e2262c4ce86cac0b0e032945f2f637d5977
Boris Vinogradov <no111u3@gmail.com>
2020-02-03 07:52:58 +0300

    Add index branching app

---
+++ src/index_branch.asm
@@ -0,0 +1,84 @@
+;****************************************************************
+.include "m48def.inc"
+.include "service.inc"
+
+;****************************************************************
+; Data
+;****************************************************************
+.dseg
+
+;****************************************************************
+; Code
+;****************************************************************
+.cseg
+.org 0x000
+		rjmp reset 	; Reset Handler
+		reti 		;		rjmp EXT_INT0 ; IRQ0 Handler
+		reti 		;		rjmp EXT_INT1 ; IRQ1 Handler
+		reti 		;		rjmp PCINT0 ; PCINT0 Handler
+		reti 		;		rjmp PCINT1 ; PCINT1 Handler
+		reti 		;		rjmp PCINT2 ; PCINT2 Handler
+		reti 		;		rjmp WDT ; Watchdog Timer Handler
+		reti 		;		rjmp TIM2_COMPA ; Timer2 Compare A Handler
+		reti 		;		rjmp TIM2_COMPB ; Timer2 Compare B Handler
+		reti 		;		rjmp TIM2_OVF ; Timer2 Overflow Handler
+		reti 		;		rjmp TIM1_CAPT ; Timer1 Capture Handler
+		reti 		;		rjmp TIM1_COMPA ; Timer1 Compare A Handler
+		reti 		;		rjmp TIM1_COMPB ; Timer1 Compare B Handler
+		reti 		;		rjmp TIM1_OVF ; Timer1 Overflow Handler
+		reti 		;		rjmp TIM0_COMPA ; Timer0 Compare A Handler
+		reti 		;		rjmp TIM0_COMPB ; Timer0 Compare B Handler
+		reti 		;		rjmp TIM0_OVF ; Timer0 Overflow Handler
+		reti 		;		rjmp SPI_STC ; SPI Transfer Complete Handler
+		reti 		;		rjmp USART_RXC ; USART, RX Complete Handler
+		reti 		;		rjmp USART_UDRE ; USART, UDR Empty Handler
+		reti 		;		rjmp USART_TXC ; USART, TX Complete Handler
+		reti 		;		rjmp ADC ; ADC Conversion Complete Handler
+		reti 		;		rjmp EE_RDY ; EEPROM Ready Handler
+		reti 		;		rjmp ANA_COMP ; Analog Comparator Handler
+		reti 		;		rjmp TWI ; 2-wire Serial Interface Handler
+		reti 		;		rjmp SPM_RDY ; Store Program Memory Ready Handler
+reset:
+		sset RAMEND
+start:
+
+		lsl r20		; Multiple r20 by 2
+
+		ldi zl, low(Table*2) ; load table to index register
+		ldi zh, high(Table*2)
+
+		clr r21		; Clear r20 to zero
+		add zl, r20 ; Add lower byte of address
+		adc zh, r21 ; Add higher byte of address
+
+		lpm r20, z+ ; load to r20 address from table
+		lpm r21, z
+
+		movw zh:zl,r21:r20 ; store address to Z
+
+		ijmp		; jump to needed selector
+
+Way0:
+		nop
+Way1:
+		nop
+Way2:
+		nop
+Way3:
+		nop
+Way4:
+		nop
+
+		rjmp out_of_here
+Table:
+		.dw Way0, Way1, Way2, Way3, Way4
+
+out_of_here:
+		nop
+
+		rjmp pc
+
+;****************************************************************
+; Eeprom Data
+;****************************************************************
+.eseg
