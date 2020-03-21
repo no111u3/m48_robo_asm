104f3a251f9f2f3a6971af5d61841f8108555d5b
Boris Vinogradov <no111u3@gmail.com>
2020-03-21 17:23:33 +0300

    Change base template project to use system headers and types

--- src/empty.asm
+++ src/empty.asm
@@ -1,66 +1,41 @@
-;****************************************************************
+;******************************************************************************
 .include "m48def.inc"
-.include "service.inc"
+.include "std/ram.inc"
+.include "std/stack.inc"
+.include "std/vectors.inc"
 
-;****************************************************************
+;******************************************************************************
 ; Data
-;****************************************************************
+;******************************************************************************
 .dseg
-.org SRAM_START ; because avra ignores device specific segment placement address
 
-;****************************************************************
+;******************************************************************************
 ; Code
-;****************************************************************
+;******************************************************************************
 .cseg
 
-; Interrupt vector table ========================================
-.org 0x000
-		rjmp reset 	; Reset Handler
-		reti 		;		rjmp EXT_INT0 ; IRQ0 Handler
-		reti 		;		rjmp EXT_INT1 ; IRQ1 Handler
-		reti 		;		rjmp PCINT0 ; PCINT0 Handler
-		reti 		;		rjmp PCINT1 ; PCINT1 Handler
-		reti 		;		rjmp PCINT2 ; PCINT2 Handler
-		reti 		;		rjmp WDT ; Watchdog Timer Handler
-		reti 		;		rjmp TIM2_COMPA ; Timer2 Compare A Handler
-		reti 		;		rjmp TIM2_COMPB ; Timer2 Compare B Handler
-		reti 		;		rjmp TIM2_OVF ; Timer2 Overflow Handler
-		reti 		;		rjmp TIM1_CAPT ; Timer1 Capture Handler
-		reti 		;		rjmp TIM1_COMPA ; Timer1 Compare A Handler
-		reti 		;		rjmp TIM1_COMPB ; Timer1 Compare B Handler
-		reti 		;		rjmp TIM1_OVF ; Timer1 Overflow Handler
-		reti 		;		rjmp TIM0_COMPA ; Timer0 Compare A Handler
-		reti 		;		rjmp TIM0_COMPB ; Timer0 Compare B Handler
-		reti 		;		rjmp TIM0_OVF ; Timer0 Overflow Handler
-		reti 		;		rjmp SPI_STC ; SPI Transfer Complete Handler
-		reti 		;		rjmp USART_RXC ; USART, RX Complete Handler
-		reti 		;		rjmp USART_UDRE ; USART, UDR Empty Handler
-		reti 		;		rjmp USART_TXC ; USART, TX Complete Handler
-		reti 		;		rjmp ADC ; ADC Conversion Complete Handler
-		reti 		;		rjmp EE_RDY ; EEPROM Ready Handler
-		reti 		;		rjmp ANA_COMP ; Analog Comparator Handler
-		reti 		;		rjmp TWI ; 2-wire Serial Interface Handler
-		reti 		;		rjmp SPM_RDY ; Store Program Memory Ready Handler
+; Interrupt vector table ======================================================
+.define reti_dummy_use
+		int_vectors
 
+; Reset Handler ===============================================================
+reset_handler:
+		setup_stack RAMEND ; Setup stack to the end of ram
+		clear_ram_regs SRAM_START, RAMEND+1 ; Clear ram and registers
+; Internal hardware init ======================================================
+;==============================================================================
 
-; Reset Handler =================================================
-reset:
-		sset RAMEND ; Setup stack to the end of ram
-		regram_clear SRAM_START, RAMEND+1 ; Clear ram and registers
-; Internal hardware init ========================================
-;================================================================
+; Eternal hardware init =======================================================
+;==============================================================================
 
-; Eternal hardware init =========================================
-;================================================================
-
-; Run ===========================================================
+; Run =========================================================================
 start:
 
-; Infinity end loop =============================================
+; Infinity end loop ===========================================================
 		rjmp pc
-;================================================================
+;==============================================================================
 
-;****************************************************************
+;******************************************************************************
 ; Eeprom Data
-;****************************************************************
+;******************************************************************************
 .eseg
